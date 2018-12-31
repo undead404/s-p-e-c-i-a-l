28506182db7edd1f950a8f2de9e2ae3ba416c20d
Vitalii Perehonchuk <brute18@gmail.com>
2018-12-31 13:56:47 +0200

    fix constrait conflict check

--- src/functions/no-constraint-conflict.js
+++ src/functions/no-constraint-conflict.js
@@ -1,16 +1,19 @@
-const limits = {
-    s: [-Infinity, Infinity],
-    p: [-Infinity, Infinity],
-    e: [-Infinity, Infinity],
-    c: [-Infinity, Infinity],
-    i: [-Infinity, Infinity],
-    a: [-Infinity, Infinity],
-    l: [-Infinity, Infinity],
-};
 const LOWER = 0;
 const UPPER = 1;
 
 export default function noConstraintConflict(constraints) {
+    const limits = {
+        s: [-Infinity, Infinity],
+        p: [-Infinity, Infinity],
+        e: [-Infinity, Infinity],
+        c: [-Infinity, Infinity],
+        i: [-Infinity, Infinity],
+        a: [-Infinity, Infinity],
+        l: [-Infinity, Infinity],
+    };
+    if (constraints.length < 2) {
+        return true;
+    }
     for (let constraint of constraints) {
         if (constraint.from) {
             if (limits[constraint.property][UPPER] < constraint.from) {
