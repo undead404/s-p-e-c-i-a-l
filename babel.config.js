989bc7a44b8be68af319c36bded3463aefac6ac2
Vitalii Perehonchuk <brute18@gmail.com>
2019-01-01 14:39:06 +0200

    added better linting

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,26 @@
 module.exports = {
+  ignore: ['node_modules', 'build'],
+  plugins: [
+    "module:@babel/plugin-proposal-class-properties", ["module:@babel/plugin-proposal-decorators", {
+      legacy: true
+    }],
+    "module:@babel/plugin-proposal-export-namespace-from",
+    "module:@babel/plugin-proposal-function-sent",
+    "module:@babel/plugin-proposal-json-strings",
+    "module:@babel/plugin-proposal-numeric-separator",
+    "module:@babel/plugin-proposal-throw-expressions",
+    "module:@babel/plugin-syntax-dynamic-import",
+    "module:@babel/plugin-syntax-import-meta",
+  ],
   presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
     '@vue/app'
   ]
 }
